6fbc5effb017623d7ea9a19086a42696eaba2076
Shawn Kemp <shawn.kemp@gmail.com>
2024-11-28 17:19:04 -0800

    updates

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ document.body.innerHTML = '<style>body {margin: 0px;text-align: center;}</style>
 setquery("fxhash",$fx.hash);
 var initialTime = new Date().getTime();
 
+//file name 
+var fileName = $fx.hash;
+
 var canvas = document.getElementById("myCanvas");
 
 paper.setup('myCanvas');
@@ -26,11 +29,29 @@ noise.noiseSeed(seed);
 
 definitions = [
     {
+        id: "layers",
+        name: "Layers",
+        type: "number",
+        default: 12,
+        options: {
+            min: 6,
+            max: 24,
+            step: 1,
+        },  
+    },
+    {
         id: "orientation",
         name: "Orientation",
         type: "select",
         default: "portrait",
-        options: {options: ["portrait", "landscape", "square"]},
+        options: {options: ["portrait", "landscape"]},
+    },
+    {
+        id: "aspectratio",
+        name: "Aspect ratio",
+        type: "select",
+        default: "4:5",
+        options: {options: ["1:1", "2:5","3:5","4:5","54:86","296:420"]},
     },
     {
         id: "size",
@@ -165,6 +186,17 @@ definitions = [
             step: .001,
         },  
     },
+    {
+        id: "matwidth",
+        name: "Mat size",
+        type: "number",
+        default: 75,
+        options: {
+            min: 50,
+            max: 200,
+            step: 10,
+        },  
+    },
 
     ]
 
@@ -172,7 +204,7 @@ definitions = [
 
 $fx.params(definitions)
 var scale = $fx.getParam('size');
-var stacks = 12;
+var stacks = $fx.getParam('layers');
 var numofcolors = $fx.getParam('colors');
 var numberofcircles=$fx.getParam("number_ripples");
 var meshDensity = $fx.getParam("density");
@@ -215,13 +247,18 @@ var xoss = $fx.getParam('yoss');
 var wide = 800; 
 var high = 1000; 
 
+if ($fx.getParam('aspectratio')== "1:1"){wide = 800; high = 800};
+if ($fx.getParam('aspectratio')== "2:5"){wide = 400; high = 1000};
+if ($fx.getParam('aspectratio')== "3:5"){wide = 600; high = 1000};
+if ($fx.getParam('aspectratio')== "4:5"){wide = 800; high = 1000};
+if ($fx.getParam('aspectratio')== "54:86"){wide = 540; high = 860};
+if ($fx.getParam('aspectratio')== "296:420"){wide =705; high = 1000};
+
+
 
 var ratio = 1/scale;//use 1/4 for 32x40 - 1/3 for 24x30 - 1/2 for 16x20 - 1/1 for 8x10
 var minOffset = ~~(7*ratio); //this is aproximatly .125"
-//var framewidth = ~~(R.random_int(125, 125)*ratio); 
-if (scale==1){var framewidth = 75};
-if (scale==2){var framewidth = ~~(125*ratio)};
-if (scale==3){var framewidth = ~~(175*ratio)};
+var framewidth = ~~($fx.getParam('matwidth')*ratio*scale); 
 var framradius = 0;
 
 
@@ -354,7 +391,7 @@ for (z = 0; z < stacks; z++) {
     //$fx.preview();
 
     
-    floatingframe();
+    //floatingframe();
     //send features to upspire.studio
     //upspirestudio(features)
 
@@ -597,6 +634,7 @@ function floatingframe(){
         var framegroup = new Group(woodframe);
         woodframe.style = {fillColor: frameColor, strokeColor: frameColor, strokeWidth: 1*ratio,shadowColor: new Color(0,0,0,[0.5]),shadowBlur: 20,shadowOffset: new Point(10*2.2, 10*2.2)};
     } else {woodframe.removeChildren()} 
+    fileName = "Framed-"+$fx.hash;
 }
 
 
